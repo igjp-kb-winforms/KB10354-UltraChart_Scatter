416e6b18b60315c0bde49704b5b35c3eaa2a9745
mkamiishi <mkamiishi@infragistics.com>
2023-01-18 17:22:43 +0900

    初回コミット
    Initial commit

---
+++ UltraChart_Scatter/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Infragistics.UltraChart.Shared.Styles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraChart_Scatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ultraChart1.ChartType = ChartType.ScatterChart;
+
+            BindingList<DataPoint> list = new BindingList<DataPoint>();
+            list.Add(new DataPoint() { Name = "テスト1", Value1 = 4, Value2 = 5 });
+            list.Add(new DataPoint() { Name = "テスト2", Value1 = 4, Value2 = 7 });
+            list.Add(new DataPoint() { Name = "テスト3", Value1 = 7, Value2 = 4 });
+            list.Add(new DataPoint() { Name = "テスト4", Value1 = 2, Value2 = 6 });
+            list.Add(new DataPoint() { Name = "テスト5", Value1 = 3, Value2 = 2 });
+
+            ultraChart1.ScatterChart.ColumnX = 1;
+            ultraChart1.ScatterChart.ColumnY = 2;
+
+            ultraChart1.DataSource = list;
+            ultraChart1.DataBind();
+        }
+
+        public class DataPoint
+        {
+            public string Name { get; set; }
+            public int Value1 { get; set; }
+            public int Value2 { get; set; }
+        }
+    }
+}
---
+++ UltraChart_Scatter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("UltraChart_Scatter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UltraChart_Scatter")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("78f1ead9-56f5-4547-b8de-64bdb827efdb")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
